d90a9d88a7c3bbb93e9367ab5a582b73bc81d20b
N1A1voIt <120253454+N1A1voIt@users.noreply.github.com>
2024-12-17 14:21:48 +0300

    ok

--- Model/User.cs
+++ Model/User.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace cloud.Model;
 
-[Table("user")]
+[Table("user_cloud")]
 public class User {
     [Key]
     [Column("id_user")]
@@ -14,7 +14,7 @@ public class User {
 
     [Column("username")]
     public string Username { get; set; }
-
+ 
     [Column("password")]
     public string Password { get; set; }
     [Column("nb_tentative")]
